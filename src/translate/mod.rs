7500c6f39f99f0e7c67e8e9984649998523ad238
ralismark <timmy.yao@outlook.com.au>
2020-10-09 13:47:30 +1100

    Support shellescape

--- src/translate/mod.rs
+++ src/translate/mod.rs
@@ -4,6 +4,10 @@
 
 pub mod stringpool;
 
+use crate::errors::Result;
+use std::process::Command;
+pub use stringpool::PoolString;
+
 type Utf16 = u16;
 type StrNumber = i32;
 type PoolPointer = i32;
@@ -18,4 +22,24 @@ extern "C" {
     pub static pool_ptr: PoolPointer;
     pub static str_ptr: StrNumber;
 
+}
+
+fn run_system_from_str(cmd: &str) -> Result<()> {
+    let mut args = cmd.split_whitespace();
+    Command::new(args.next().ok_or("no command given")?)
+        .args(args)
+        .spawn()?
+        .wait()?;
+
+    Ok(())
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn run_system(len: usize) -> bool {
+    let pstr = PoolString::from_strptr_with_len(len);
+    let pslice = &pstr.as_slice()[0..len];
+
+    // Might want to change this to non-lossy...
+    let command = String::from_utf16_lossy(pslice);
+    run_system_from_str(&command).is_ok()
 }
